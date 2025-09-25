26bb4f6365ec8f5dcdbac3817fb6bb76b0955448
batireedui <batireeduiotgonsukh@gmail.com>
2025-09-25 16:08:10 +0800

    s

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "burtgel.steam.mn",
-  description: "burtgel.mn",
+  description: "burtgel.steam.mn",
 };
 
 export default function RootLayout({
